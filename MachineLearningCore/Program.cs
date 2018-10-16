af6b2fee61875272e1f96475713f3657731a0c43
Jonathan Gomez G <jg05370@outlook.com>
2018-10-15 20:47:40 -0600

    Agregado codigo para red neuronal Mi idea para el proximo ejemplo es implementar un algoritmo clasificatorio

--- MachineLearningCore/Program.cs
+++ MachineLearningCore/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Models;
+using Microsoft.ML.Legacy.Data;
+using Microsoft.ML.Legacy.Transforms;
+using Microsoft.ML.Legacy.Trainers;
+using Microsoft.ML.Runtime.Api;
 
 namespace MachineLearningCore
 {
     class Program
     {
-        static void Main(string[] args)
+        //You need to create three global fields to hold the paths to the recently downloaded files:
+
+        //_dataPath has the path to the dataset used to train the model.
+        //_testDataPath has the path to the dataset used to evaluate the model.
+        //_modelPath has the path where the trained model is saved.
+
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-data.tsv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "wikipedia-detox-250-line-test.tsv");
+        static readonly string _modelpath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        public class SentimentData
         {
-            Console.WriteLine("Hello World!");
+            [Column(ordinal: "0", name: "Label")]
+            public float Sentiment;
+            [Column(ordinal: "1")]
+            public string SentimentText;
         }
+
+        public class SentimentPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public bool Sentiment;
+        }
+
+        static async Task Main(string[] args)
+        {
+            var model = await Train();
+            Evaluate(model);
+            Predict(model);
+            Console.ReadLine();
+        }
+        public static async Task<PredictionModel<SentimentData, SentimentPrediction>> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_dataPath).CreateFrom<SentimentData>());
+            pipeline.Add(new TextFeaturizer("Features", "SentimentText"));
+            pipeline.Add(new FastForestBinaryClassifier() { NumLeaves = 50, NumTrees = 50, MinDocumentsInLeafs = 20 });
+            PredictionModel<SentimentData, SentimentPrediction> model = pipeline.Train<SentimentData, SentimentPrediction>();
+            await model.WriteAsync(_modelpath);
+            return model;
+        }
+
+        public static void Evaluate(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            var testData = new TextLoader(_testDataPath).CreateFrom<SentimentData>();
+            var evaluator = new BinaryClassificationEvaluator();
+            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
+            Console.WriteLine();
+            Console.WriteLine("PredictionModel quality metrics evaluation");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+        }
+
+        public static void Predict(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            IEnumerable<SentimentData> sentiments = new[]
+            {
+                new SentimentData
+                {
+                    SentimentText = "Please refrain from adding nonsense to Wikipedia."
+                },
+                new SentimentData
+                {
+                    SentimentText = "He is the best, and the article should say that."
+                }
+            };
+            IEnumerable<SentimentPrediction> predictions = model.Predict(sentiments);
+            Console.WriteLine();
+            Console.WriteLine("Sentiment Predictions");
+            Console.WriteLine("---------------------");
+            var sentimentsAndPredictions = sentiments.Zip(predictions, (sentiment, prediction) => (sentiment, prediction));
+            foreach (var item in sentimentsAndPredictions)
+            {
+                Console.WriteLine($"Sentiment: {item.sentiment.SentimentText} | Prediction: {(item.prediction.Sentiment ? "Negative" : "Positive")}");
+            }
+            Console.WriteLine();
+
+
+        }
+
     }
 }
